9d5b2ea3d7ade732c1f094f71208b126c4641928
sara1404 <sara14042000@gmail.com>
2022-04-06 22:41:56 +0200

    main window created

--- Project/hospital/hospital/MainWindow.xaml.cs
+++ Project/hospital/hospital/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace hospital
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

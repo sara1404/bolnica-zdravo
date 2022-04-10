93e09f45e17a817c2f85b7a83597ceb1817bb0c0
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-04-10 18:49:19 +0200

    added serialization of patients

--- Project/hospital/hospital/FileHandler/PatientFileHandler.cs
+++ Project/hospital/hospital/FileHandler/PatientFileHandler.cs
@@ -6,16 +6,26 @@ namespace FileHandler
 {
     public class PatientFileHandler
     {
-        private readonly string path;
+        private readonly string path = @"../../Resources/Data/patientData.txt";
 
         public List<Patient> Read()
         {
-            throw new NotImplementedException();
+            try
+            {
+                string serializedPatients = System.IO.File.ReadAllText(path);
+                List<Patient> patients = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Patient>>(serializedPatients);
+                return patients;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
-        public bool Write(List<Patient> patients)
+        public void Write(List<Patient> patients)
         {
-            throw new NotImplementedException();
+            string serializedPatients = Newtonsoft.Json.JsonConvert.SerializeObject(patients);
+            System.IO.File.WriteAllText(path, serializedPatients);
         }
 
     }

ebe537c1856842c7d7672a52d79a7270314df7c3
sara1404 <sara14042000@gmail.com>
2022-04-10 17:42:43 +0200

    bug fixed and class diagram updated

--- Project/hospital/hospital/App.xaml.cs
+++ Project/hospital/hospital/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Controller;
 using Repository;
 using Service;
+using System;
 using System.Windows;
 
 namespace hospital
@@ -10,14 +11,16 @@ namespace hospital
     /// </summary>
     public partial class App : Application
     {
+        public RoomRepository roomRepository;
         public RoomController roomController { get; set; }
         public PatientController patientController { get; set; }
         public AppointmentController appointmentController { get; set; }
         public MedicalRecordsController mediicalRecordsController { get; set; }
         public DoctorController doctorController { get; set; }
-        
-        public App() {
-            RoomRepository roomRepository = new RoomRepository();
+
+        public App()
+        {
+            roomRepository = new RoomRepository();
             RoomService roomService = new RoomService(roomRepository);
             roomController = new RoomController(roomService);
 
@@ -36,6 +39,13 @@ namespace hospital
 
             DoctorService doctorService = new DoctorService(doctorRepository);
             doctorController = new DoctorController(doctorService);
+
+            roomRepository.LoadRoomData();
+        }
+
+        private void App_Closing(object sender, ExitEventArgs e)
+        {
+            roomRepository.WriteRoomData();
         }
     }
 }
--- Project/hospital/hospital/MainWindow.xaml.cs
+++ Project/hospital/hospital/MainWindow.xaml.cs
@@ -33,5 +33,7 @@ namespace hospital
         {
             new ManagerMainWindow().Show();
         }
+
+     
     }
 }

9c7b9eaf7b67d3c68ebd55915a3a761ba2463855
sara1404 <sara14042000@gmail.com>
2022-05-02 22:08:37 +0200

    bug in relocation fixed

--- Project/hospital/hospital/View/ManagerMainWindow.xaml.cs
+++ Project/hospital/hospital/View/ManagerMainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Controller;
+using System.Windows;
 
 namespace hospital.View
 {
@@ -7,9 +8,12 @@ namespace hospital.View
     /// </summary>
     public partial class ManagerMainWindow : Window
     {
+        private UserController uc;
         public ManagerMainWindow()
         {
             InitializeComponent();
+            App app = Application.Current as App;
+            uc = app.userController;
         }
 
         private void Room_Button_Click(object sender, RoutedEventArgs e)
@@ -31,5 +35,19 @@ namespace hospital.View
         {
             Main.Content = new RoomRenovationPage();
         }
+
+        private void Main_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            uc.CurentLoggedUser = null;
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            this.Close();
+        }
+
     }
 }

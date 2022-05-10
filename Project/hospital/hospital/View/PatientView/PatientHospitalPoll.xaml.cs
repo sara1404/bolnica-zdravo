510e0704d312c50c79c27b83774b93666fc47c81
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-05-10 14:47:13 +0200

    Extract data from frontend and check data

--- Project/hospital/hospital/View/PatientView/PatientHospitalPoll.xaml.cs
+++ Project/hospital/hospital/View/PatientView/PatientHospitalPoll.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using hospital.Controller;
+using hospital.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,13 +24,17 @@ namespace hospital.View.PatientView
     public partial class PatientHospitalPoll : Page
     {
         private App app;
+        public List<PollQuestion> Poll { get; set; }
         public PatientHospitalPoll()
         {
             InitializeComponent();
             app = Application.Current as App;
             PollBlueprintController pbc = app.pollBlueprintController;
-            pollTable.ItemsSource = pbc.GetHospitalPoll().PollQuestions;
-            AddComboboxColumn();
+            //lbPoll.ItemsSource = pbc.GetHospitalPoll().PollQuestions;
+            DataContext = this;
+            Poll = pbc.GetHospitalPoll().PollQuestions;
+            //AddComboboxColumn();
+            //this.Resources.Add("poll", pbc.GetHospitalPoll().PollQuestions);
         }
         private void AddComboboxColumn()
         {
@@ -37,7 +43,7 @@ namespace hospital.View.PatientView
             dgcc.ItemsSource = grades;
             dgcc.Width = 50;
             dgcc.DisplayIndex = 3;
-            pollTable.Columns.Add(dgcc);
+            //pollTable.Columns.Add(dgcc);
         }
 
         private void btnBack_Click(object sender, RoutedEventArgs e)
@@ -47,8 +53,60 @@ namespace hospital.View.PatientView
 
         private void btnConfirm_Click(object sender, RoutedEventArgs e)
         {
-            //foreach()
-            Console.WriteLine();
+            if (!CheckIfFilled())
+            {
+                MessageBox.Show("Please answer all of the questions!");
+            }
+            else
+            {
+
+            }
+            
+
+        }
+
+        private bool CheckIfFilled()
+        {
+            foreach (var listIterator in lbPoll.Items)
+            {
+                ListBoxItem item = (ListBoxItem)lbPoll.ItemContainerGenerator.ContainerFromItem(listIterator);
+                ContentPresenter presenter = FindVisualChild<ContentPresenter>(item);
+                DataTemplate dataTemplate = presenter.ContentTemplate;
+                if (dataTemplate != null)
+                {
+                    bool oneChecked = (bool)((RadioButton)dataTemplate.FindName("rbOne", presenter)).IsChecked;
+                    bool twoChecked = (bool)((RadioButton)dataTemplate.FindName("rbTwo", presenter)).IsChecked;
+                    bool threeChecked = (bool)((RadioButton)dataTemplate.FindName("rbThree", presenter)).IsChecked;
+                    bool fourChecked = (bool)((RadioButton)dataTemplate.FindName("rbFour", presenter)).IsChecked;
+                    bool fiveChecked = (bool)((RadioButton)dataTemplate.FindName("rbFive", presenter)).IsChecked;
+                    if (!oneChecked && !twoChecked && !threeChecked && !fourChecked && !fiveChecked)
+                    {
+                        return false;
+                    }
+                    
+                }
+            }
+            return true;
+        }
+        // this function has been found online so DON'T F**ING TOUCH IT 
+        private childItem FindVisualChild<childItem>(DependencyObject obj)
+        where childItem : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is childItem)
+                {
+                    return (childItem)child;
+                }
+                else
+                {
+                    childItem childOfChild = FindVisualChild<childItem>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
         }
     }
 }

0257ce8af4588d26a0864db5b0157c841ec23c25
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-05-14 18:35:39 +0200

    Fix appointment id problem

--- Project/hospital/hospital/Repository/AppointmentRepository.cs
+++ Project/hospital/hospital/Repository/AppointmentRepository.cs
@@ -36,9 +36,22 @@ namespace Repository
         public int GetNewId()
         {
             if (appointments.Count == 0)
+            {
                 return 0;
-            else 
-                return appointments[appointments.Count - 1].Id + 1;
+            }
+            else
+            {
+                int max = 0;
+                foreach(Appointment a in appointments)
+                {
+                    if(a.Id > max)
+                    {
+                        max = a.Id;
+                    }
+                }
+                return max + 1;
+            }
+                
         }
         public Appointment FindById(int id)
         {

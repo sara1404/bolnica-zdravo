fbc147272ac2c575b710e7afe0eb223e1ffb0379
Bosko Ristovic <malibolence@gmail.com>
2022-05-26 23:45:36 +0200

    add feature DoctorVacationViewWindow

---
+++ Project/hospital/hospital/View/DoctorVacationViewWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Controller;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace hospital.View
+{
+    /// <summary>
+    /// Interaction logic for DoctorVacationViewWindow.xaml
+    /// </summary>
+    public partial class DoctorVacationViewWindow : Window
+    {
+        private VacationRequestController vc;
+
+        private VacationRequest selectedVacation;
+        public DoctorVacationViewWindow()
+        {
+            InitializeComponent();
+            App app = Application.Current as App;
+            vc = app.vacationRequestController;
+            this.DataContext = this;
+            foreach (Window window in Application.Current.Windows)
+            {
+                if (window.GetType() == typeof(DoctorVacationWindow))
+                {
+                    selectedVacation = (window as DoctorVacationWindow).tableRequests.SelectedItem as VacationRequest;
+                    lbStatus.Content = selectedVacation.Status;
+                    tbNote.Text = selectedVacation.Note;
+                }
+            }
+
+        }
+    }
+}

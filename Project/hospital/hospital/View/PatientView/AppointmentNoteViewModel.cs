935f609c47af9fdedf552b550afe6e36cf4d7ca1
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-05-30 18:01:28 +0200

    Transfer patient leave note page to MVVM

---
+++ Project/hospital/hospital/View/PatientView/AppointmentNoteViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Controller;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace hospital.View.PatientView
+{
+    public class AppointmentNoteViewModel
+    {
+        public string DoctorNote { get; set;}
+        public string PatientNote { get; set; }
+        public MyICommand ConfirmCommand { get; set; }
+        private AppointmentController ac;
+        private Appointment oldAppointment;
+
+        public AppointmentNoteViewModel(Appointment appointment)
+        {
+            App app = Application.Current as App;
+            ac = app.appointmentController;
+            ConfirmCommand = new MyICommand(Confirm);
+            DoctorNote = appointment.DoctorNote;
+            PatientNote = appointment.PatientNote;
+            oldAppointment = appointment;
+        }
+
+        private void Confirm()
+        {
+            Appointment newAppointment = oldAppointment;
+            newAppointment.PatientNote = PatientNote;
+            ac.UpdateAppointment(oldAppointment, newAppointment);
+        }
+
+    }
+}

bf156dab0620562052d0f4d056b59f4c0041cf36
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-06-10 17:05:02 +0200

    Refactor patient calendar so that it uses MVVM

--- Project/hospital/hospital/View/PatientView/PatientCalendar.xaml.cs
+++ Project/hospital/hospital/View/PatientView/PatientCalendar.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Controller;
-using Model;
-using Syncfusion.UI.Xaml.Scheduler;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Windows.Controls;
 
 namespace hospital.View.PatientView
 {
@@ -14,21 +10,7 @@ namespace hospital.View.PatientView
         public PatientCalendar()
         {
             InitializeComponent();
-            App app = Application.Current as App;
-            AppointmentManagementController ac = app.appointmentController;
-            ScheduleAppointmentCollection sac = new ScheduleAppointmentCollection();
-
-            foreach (Appointment a in ac.GetAppointmentByPatient(app.userController.CurentLoggedUser.Username))
-            {
-                ScheduleAppointment sa = new ScheduleAppointment();
-                sa.StartTime = a.StartTime;
-                sa.EndTime = a.StartTime.AddMinutes(30);
-                sa.IsAllDay = false;
-                sa.Subject = a.DoctorUsername;
-                sac.Add(sa);
-            }
-            appointmentCalendar.ItemsSource = sac;
-            
+            DataContext = new PatientCalendarViewModel();
         }
     }
 }

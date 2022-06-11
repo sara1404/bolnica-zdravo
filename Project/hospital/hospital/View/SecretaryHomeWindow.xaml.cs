9664ed6772171b5e29f5349444d2d686e372e77e
unknown <kalinic.ra71.2019@uns.ac.rs>
2022-06-11 09:49:37 +0200

    changed color of side bar in secetary

--- Project/hospital/hospital/View/SecretaryHomeWindow.xaml.cs
+++ Project/hospital/hospital/View/SecretaryHomeWindow.xaml.cs
@@ -25,7 +25,10 @@ namespace hospital.View
             this.DataContext = this;
             App app = Application.Current as App;
             uc = app.userController;
-            Console.WriteLine(DateTime.Now.ToString());
+
+            btnhandlingAccount.BorderBrush = (Brush)(new BrushConverter().ConvertFrom("#c8d8e4"));
+            btnhandlingAccount.BorderThickness = new Thickness(3, 0, 0, 0);
+            handlingAccountUserControl.Visibility = Visibility.Visible;
         }
 
         private void handlingAccount_Click(object sender, RoutedEventArgs e)

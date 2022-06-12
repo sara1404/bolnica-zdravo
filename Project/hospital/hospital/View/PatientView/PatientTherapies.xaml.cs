90c577f5a17a9accdb25e8a0fa781382b2e01de5
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2022-06-12 16:14:29 +0200

    Modify patient therapies codebehind

--- Project/hospital/hospital/View/PatientView/PatientTherapies.xaml.cs
+++ Project/hospital/hospital/View/PatientView/PatientTherapies.xaml.cs
@@ -1,22 +1,5 @@
-﻿using Controller;
-using hospital.DTO;
-using hospital.View.PatientView.ViewModel;
-using Model;
-using Syncfusion.Pdf;
-using Syncfusion.Pdf.Graphics;
-using Syncfusion.Pdf.Tables;
-using Syncfusion.UI.Xaml.Scheduler;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Drawing;
-using System.Windows;
+﻿using hospital.View.PatientView.ViewModel;
 using System.Windows.Controls;
-using System.Windows.Media;
-using ToastNotifications;
-using ToastNotifications.Lifetime;
-using ToastNotifications.Messages;
-using ToastNotifications.Position;
 
 namespace hospital.View.PatientView
 {
